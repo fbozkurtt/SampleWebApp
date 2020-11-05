879416d6960c5a4385051d85bc893c05829fc645
FurkanSAU <FurkanSAU@FURKANPC>
2020-11-05 15:00:59 +0300

    Add project files.

---
+++ SampleWebApp/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.Versioning;
+using System.Web.Mvc;
+
+namespace SampleWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Error(int statusCode, Exception exception)
+        {
+            Response.StatusCode = statusCode;
+            ViewBag.StatusCode = statusCode + " " + Languages.Global.error;
+            ViewBag.Message = exception.Message;
+            return View();
+        }
+    }
+}
